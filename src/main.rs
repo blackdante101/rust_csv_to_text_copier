813c402d6054ff1f8714b2f9e20cad6ca8e407a5
Abukari Einus <blackmartyr00@gmail.com>
2025-02-20 17:07:27 +0000

    added idiomatic error handling

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-use std::io::Write;
-use std::{env, process};
-use std::fs::{self, File};
-fn main() {
+use std::{
+    env,
+    fs::{self, File},
+    io::{self, Write}
+
+};
+
+fn main() -> Result<(), io::Error> {
     println!("********************************************");
     println!("*** Extract data from a CSV to text file ***");
     println!("********************************************");
 
     if env::args().len() < 2{
-        println!("Error please enter a valid argument eg. cargo run <file_path> ");
-        process::exit(0);
-    }
+       return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Error please enter a valid argument eg. cargo run <file_path>",
+        ))
+    } else{
 
-    let file_path = env::args().nth(1).unwrap();
+        let file_path = env::args().nth(1).unwrap();
 
-    let content_of_csv = fs::read_to_string(&file_path);
+        let content_of_csv = fs::read_to_string(&file_path)?;
 
-    match content_of_csv {
-        Ok(_) => {
-            println!(".....processing file contents (0/1)");
-       
-            let content_of_csv = content_of_csv.unwrap();
-            let output_file_name = "output.txt";
+        println!(".....processing file contents (0/1)");
 
-            let mut new_text_file = File::create(output_file_name).expect("Error creating file");
+        let output_file_name = "output.txt";
 
-            new_text_file.write(content_of_csv.as_bytes()).expect("Error writing to file");
+        let mut new_text_file = File::create(output_file_name)?;
 
-            println!(".....csv file has been copied successfully! (1/1)");
-            println!("{} has been copied into {}", &file_path, output_file_name);
+        new_text_file.write(content_of_csv.as_bytes())?;
 
-        },
-        Err(e) => {
-            println!("{}", e);
+        println!(".....csv file has been copied successfully! (1/1)");
+        println!("{} has been copied into {}", &file_path, output_file_name);
+
+           
         }
+        
+        Ok(())
     }
+ 
 
-}
